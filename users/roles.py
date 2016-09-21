1956ab84332a1bc6adb3f079605e264895aa4401
Piotr Zieliński <thepateto@gmail.com>
2016-09-21 21:04:39 +0200

    New rule structure

--- users/roles.py
+++ users/roles.py
@@ -1,11 +1,21 @@
 """
-Here are defined User's roles.
+Place for defining users roles.
+
+syncrules - command which creates groups based on defined here rules.
+            Also if any permission in database is diffrent than described here,
+            all will be replaced with existing in this file.
 """
 
-DRIVER = 0
-OFFICER = 1
-ROLES = (
-    (DRIVER, 'Kierowca'),
-    (OFFICER, 'Kontroler'),
-)
+Driver = {
+    'verbose': 'Kierowca',
+    'permissions': (
+        # tu uprawnienia        
+    ),
+}
 
+Officer = {
+    'verbose': 'Kontroler',
+    'permissions': (
+        # tu uprawnienia
+    ),
+}
