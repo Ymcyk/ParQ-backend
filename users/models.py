e1b3248e8e7662af1279ee21714dac60cc1e2822
Piotr Zieliński <thepateto@gmail.com>
2016-09-21 20:39:12 +0200

    Set custom ParkingUser manager to model

--- users/models.py
+++ users/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from .managers import ParkingUserManager
+
 class ParkingUser(User):
-    objects = None
+    """
+    Proxy User class for handling Parking user's
+
+    Assigned to objects ParkingUserManager assign user to specific role.
+    """
+
+    objects = ParkingUserManager
 
     class Meta:
         proxy = True
