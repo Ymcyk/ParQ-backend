25b299efeff2fa577bccf8173ed80c3400e24201
Piotr Zieliński <thepateto@gmail.com>
2016-09-28 14:23:14 +0200

    RoleUserManager is not used

--- users/models.py
+++ users/models.py
@@ -1,9 +1,6 @@
 from django.contrib.auth.models import User
 
-from .managers import RoleUserManager
-
 class RoleUser(User):
-    objects = RoleUserManager()
 
     class Meta:
         proxy = True
