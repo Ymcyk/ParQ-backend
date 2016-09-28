e1e4178529efcf256591b19e810702ec80b5515a
Piotr Zieliński <thepateto@gmail.com>
2016-09-28 18:15:31 +0200

    Changed new roles definition.
    Now new roles are defined as classes derived from BaseRole.
    Role permissions are stored in permissions atribute.

--- users/roles.py
+++ users/roles.py
@@ -1,27 +1,15 @@
 """
 Place for defining users roles.
 
-syncrules - command which creates groups based on defined here rules.
+syncroles - command which creates groups based on defined here rules.
             Also if any permission in database is diffrent than described here,
             all will be replaced with existing in this file.
 """
+from djroles.roles import BaseRole
 
-# rezygnuję z tego pomysłu, fixtures robi to samo
-Driver = {
-    'verbose': 'Kierowca',
-    'permissions': (
-        # tu uprawnienia        
-    ),
-}
+class Driver(BaseRole):
+    permissions = ()
 
-Officer = {
-    'verbose': 'Kontroler',
-    'permissions': (
-        # tu uprawnienia
-    ),
-}
+class Officer(BaseRole):
+    pass
 
-def assign_role(user, role):
-    """
-    
-    """
