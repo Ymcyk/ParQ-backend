6dc9d95d5e863239c166a2326e6c68c9fc457ef7
Piotr Zieliński <thepateto@gmail.com>
2016-10-12 00:54:40 +0200

    Badge is_assigned instead is_active and override Vehicle save method
    Now is_assigned is not property, but Bool in place is_active.
    In save method badge's is_assigned field is changed to True and
    check if badge wasn't assigned before.

--- badges/models.py
+++ badges/models.py
@@ -5,30 +5,21 @@ from django.utils.translation import ugettext as _
 
 from django_countries.fields import CountryField
 
+from .exceptions import BadgeNotAvailable
+
 class Badge(models.Model):
     """
     Randomly generated unique ID. Used to register user's vehicle with 
     scanned plate's QR code.
-    is_active - if True, than badge can be used by user.
     Objects of this model shouldn't be deleted.
     """
-    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    is_active = models.BooleanField(
-            _('is active'),
-            help_text=_('Default is True. When related vehicle was deleted, '
-                        'than is unset.'),
-            default=True,
+    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    is_assigned = models.BooleanField(
+            _('is assigned'),
+            help_text=_('Default is False. True after assigment to vehicle'),
+            default=False,
             )
     
-    @property
-    def is_assigned(self):
-        """
-        Tells if badge is assigned to any vehicle.
-        When Badge is not assigned, than Django don't create related field
-        in that object.
-        """
-        return hasattr(self, 'vehicle')
-
     def __str__(self):
         return str(self.uuid)
 
@@ -67,6 +58,14 @@ class Vehicle(models.Model):
             editable=False,
             )
 
+    def save(self, *args, **kwargs):
+        if self.badge.is_assigned:
+            raise BadgeNotAvailable('Badge was assigned to vehicle')
+
+        super(Vehicle, self).save(*args, **kwargs)
+        
+        self._set_badge_assigned()
+
     def __str__(self):
         plate_str = '{}-{}'.format(self.plate_country, self.plate_number)
         return '{} {}'.format(self.name, plate_str) if self.name else plate_str 
@@ -76,6 +75,10 @@ class Vehicle(models.Model):
         Vehicle's are equal when plate number and country is the same
         """
         return self._is_number_eq(other) and self._is_country_eq(other)  
+
+    def _set_badge_assigned(self):
+        self.badge.is_assigned = True
+        self.badge.save()
 
     def _is_number_eq(self, other):
         return self.plate_number == other.plate_number
