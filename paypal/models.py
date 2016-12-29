4987952a89f1df28836cbc112dcc60a5523dc87b
Piotr Zieliński <thepateto@gmail.com>
2016-12-29 14:13:13 +0100

    Transaction model

--- paypal/models.py
+++ paypal/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Transaction(models.Model):
+    user = models.ForeignKey(
+            User,
+            on_delete=models.CASCADE,
+            )
+    transaction_id = models.CharField(
+            max_length=28,
+            unique=True,
+            )
+
+    def __str__(self):
+        return '{}: {}'.format(self.user.username, self.transaction_id)
+
