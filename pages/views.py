c7183f186ba8ff0ee70ac4c33fae6a582523ad17
Piotr Zieliński <thepateto@gmail.com>
2017-01-13 23:31:18 +0100

    Fixes

--- pages/views.py
+++ pages/views.py
@@ -3,8 +3,6 @@ from django.views import View
 
 class Index(View):
     def get(self, request):
-        parking = Parking.objects.all()[0]
-        schedules = parking.schedule_lot.schedule_set.all()
         context = {
                 'title': 'ParQ',
                 }
