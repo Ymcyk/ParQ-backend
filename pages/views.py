87ee217afe0062f480a0a8fbf441fb1cd2eba647
Piotr Zieliński <thepateto@gmail.com>
2017-01-13 18:00:10 +0100

    Added view and template

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from parkings.models import Parking
+
+class Index(View):
+    def get(self, request):
+        parking = Parking.objects.all()[0]
+        schedules = parking.schedule_lot.schedule_set.all()
+        context = {
+                'parking': parking,
+                'schedules': schedules,
+                }
+        return render(request, 'pages/index.html', context)
+
