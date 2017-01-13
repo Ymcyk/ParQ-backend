e07c5f5f0b4f53481288385b5af06b1995d75ea5
Piotr Zieliński <thepateto@gmail.com>
2017-01-13 22:25:08 +0000

    Changes index view

--- pages/views.py
+++ pages/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render
 from django.views import View
 
-from parkings.models import Parking
-
 class Index(View):
     def get(self, request):
         parking = Parking.objects.all()[0]
         schedules = parking.schedule_lot.schedule_set.all()
         context = {
-                'parking': parking,
-                'schedules': schedules,
+                'title': 'ParQ',
                 }
         return render(request, 'pages/index.html', context)
 
